74fa315ba0373befcf7a79979a1abbe17ff43a11
anotherLostKitten <eggismagicchickensquared@gmail.com>
2025-09-17 11:55:42 -0400

    reorganized directory structure

--- cmd/anglish/main.go
+++ cmd/anglish/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/anotherLostKitten/Anglish/internal/parse"
 )
 
 func main() {
+	parse.Test()
 	fmt.Println("Hello World!")
 }
---
+++ internal/parse/parser.go
@@ -0,0 +1,7 @@
+package parse
+
+import "fmt"
+
+func Test() {
+	fmt.Println("ok contrats you printed a line")
+}
