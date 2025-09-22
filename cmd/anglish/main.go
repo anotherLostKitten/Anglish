ab24889071efcdc40eced5e233b74daa1e4b1aa8
anotherLostKitten <eggismagicchickensquared@gmail.com>
2025-09-21 22:14:19 -0400

    working on parser (little bit)

--- cmd/anglish/main.go
+++ cmd/anglish/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	// "io"
+	"strings"
 
 	"github.com/anotherLostKitten/Anglish/internal/parse"
 )
 
 func main() {
-	parse.Test()
+	r := strings.NewReader("@spacey:FUNC ( in=%argy )")
+	parse.ParseFromReader(r)
 	fmt.Println("Hello World!")
 }
